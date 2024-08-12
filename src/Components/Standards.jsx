2a135aa431a111ac382e64773fcc48906607772b
Rorisang Putu <rorisangputu@gmail.com>
2024-08-12 17:32:55 +0200

    fixing width

--- src/Components/Standards.jsx
+++ src/Components/Standards.jsx
@@ -5,16 +5,16 @@ const Standards = () => {
     return (
         <div className="w-full bg-blue-200">
             <div className="w-[90%] mx-auto py-4 font-custom flex flex-col md:flex-row items-center">
-                <div className="py-5 flex flex-col gap-8">
+                <div className="py-5 flex flex-col gap-8 h-1/2 w-full md:w-3/5 md:h-full">
                     <h1 className="text-4xl">Highest standards</h1>
-                    <p className="leading-[3vh] md:w-[70%]">
+                    <p className="leading-[3vh] md:w-[70%] md:text-xl">
                         Our team of seasoned attorneys
                         is committed to delivering tailored solutions,
                         ensuring you receive the attention and representation you deserve.
                     </p>
                 </div>
                 {/* Accordian */}
-                <div>
+                <div className="h-1/2 w-full md:w-2/5 md:h-full">
                     <Accordian />
                 </div>
             </div>
