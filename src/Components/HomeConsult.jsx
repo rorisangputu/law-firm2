6e22686355b9dd9276899d95330a094745e009b5
Rorisang Putu <rorisangputu@gmail.com>
2024-08-12 19:54:46 +0200

    consult finish

--- src/Components/HomeConsult.jsx
+++ src/Components/HomeConsult.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const HomeConsult = () => {
     return (
-        <div className='w-full'>
-            <div className='w-[90%] mx-auto font-custom'>
-                <div className='flex flex-col gap-6'>
+        <div className='w-full py-5'>
+            <div className='w-[90%] mx-auto font-custom flex flex-col lg:flex-row gap-7'>
+                <div className='flex flex-col gap-6 lg:w-1/2'>
                     <div className='flex bg-blue-100 p-5 h-[19vh] mr-5 '>
                         <div className='flex flex-col gap-4 justify-center'>
                             <div className='flex w-full justify-between items-center'>
@@ -19,7 +19,7 @@ const HomeConsult = () => {
                             </p>
                         </div>
                     </div>
-                    <div className='flex bg-blue-100 p-5 h-[19vh] ml-5'>
+                    <div className='flex bg-blue-950 p-5 h-[19vh] ml-5 text-white'>
                         <div className='flex flex-col gap-4 justify-center'>
                             <div className='flex w-full justify-between items-center'>
                                 <h1 className='text-3xl font-medium'>
@@ -47,10 +47,19 @@ const HomeConsult = () => {
                             </p>
                         </div>
                     </div>
-
                 </div>
-                <div>
-
+                <div className='lg:w-1/2 flex '>
+                    <div className='h-full flex flex-col gap-7 md:justify-center md:items-center'>
+                        <h1 className='text-3xl font-medium leading-[5vh]'>
+                            Your rights matter, we are here to defend them
+                        </h1>
+                        <p className='leading-[3vh]'>
+                            We prioritize our clients&apos; needs, understanding that each case is unique. Our team of seasoned attorneys is committed to delivering tailored solutions, ensuring you receive the attention and representation you deserve. Our comprehensive expertise allows us to handle a wide range of legal matters effectively.
+                        </p>
+                        <button className="font-custom bg-blue-100 p-4 w-[50%]">
+                            Get a free consultation
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
