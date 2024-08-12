36434987ddd0a0259472fcf40b0fb9b01e3e7f34
Rorisang Putu <rorisangputu@gmail.com>
2024-08-12 17:29:10 +0200

    general additions

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import Hero from "../Components/Hero"
 import Integrity from "../Components/Integrity"
+import Standards from "../Components/Standards"
 
 const Home = () => {
     return (
         <div className="">
             <Hero />
             <Integrity />
+            <Standards />
         </div>
     )
 }
