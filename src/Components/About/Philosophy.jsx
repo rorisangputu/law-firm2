a41d2666cbc3d4ac40468eacc0a7d4c091144ce5
Rorisang Putu <rorisangputu@gmail.com>
2024-08-14 14:51:49 +0200

    adding philosophy component

---
+++ src/Components/About/Philosophy.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Philosophy = () => {
+    return (
+        <div className='w-full'>
+            <div className='w-[90%] mx-auto font-custom'>
+                <div className='flex flex-col py-7'>
+                    <div className='flex flex-col gap-5'>
+                        <h1 className='text-2xl'>Our Philosophy</h1>
+                        <div className='flex flex-col gap-3'>
+                            <p>
+                                At In Law, we believe that legal counsel should be proactive, not reactive.
+                                Our mission is to provide comprehensive legal support tailored to the unique
+                                needs of entrepreneurs, ensuring that you can focus on growing your business
+                                with the confidence that your legal matters are expertly managed.
+                            </p>
+                            <p>
+                                We pride ourselves on being both approachable and professional, with a deep
+                                commitment to educating our clients. We empower you with the knowledge necessary
+                                to make informed decisions, so you always understand the legal landscape and the
+                                best path forward for your business. Our firm was built on the principle that
+                                the client&apos;s needs come first, and we strive to offer the kind of legal experience
+                                we would expect if we were in your shoes.
+                            </p>
+                        </div>
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Philosophy
--- src/Pages/TheFirm.jsx
+++ src/Pages/TheFirm.jsx
@@ -1,11 +1,12 @@
 import AboutHero from "../Components/About/AboutHero"
+import Philosophy from "../Components/About/Philosophy"
 
 
 const TheFirm = () => {
     return (
         <div className="w-full">
             <AboutHero />
-
+            <Philosophy />
         </div>
     )
 }
