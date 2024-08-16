3d5802a500af99c991d5c663c314f5c713ba2e3b
Rorisang Putu <rorisangputu@gmail.com>
2024-08-16 02:19:11 +0200

    changing button

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -31,7 +31,7 @@ const Nav = () => {
                         </li>
                         <li>
                             <Link to={'/contacts'}>
-                                <div className='border border-black rounded-md p-1'>
+                                <div className='hover:border bg-black text-white hover:bg-white hover:text-black hover:border-black  p-1'>
                                     Get in touch
                                 </div>
                             </Link>
