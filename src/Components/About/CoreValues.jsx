b3d85bd6a306e4430eddb792d81538990c8fb9d6
Rorisang Putu <rorisangputu@gmail.com>
2024-10-06 03:40:01 +0200

    updating spacing

--- src/Components/About/CoreValues.jsx
+++ src/Components/About/CoreValues.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
+
 import AccordionAbout from './AccordionAbout';
 
 const CoreValues = () => {
     return (
-        <div className='w-full bg-blue-100'>
-            <div className='w-[90%] mx-auto font-custom text-black py-10'>
+        <div className='w-full bg-blue-100 py-10 lg:py-20'>
+            <div className='w-[90%] mx-auto font-custom text-black py-5'>
                 <div className='flex flex-col lg:flex-row gap-4'>
-                    <div className='flex flex-col gap-5'>
+                    <div className='flex flex-col justify-center gap-5'>
                         <h1 className='text-2xl md:text-4xl'>Our Core Values</h1>
                         <p>
                             Guided by our fundamental principles, we embrace a set of core values
